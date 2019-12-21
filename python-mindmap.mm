a882da508985789b2da9abb6d48eafa13decde68
Philippe Crama <pcfeb0009@gmx.com>
2019-12-21 15:09:11 +0100

    Mindmap with some Python concepts

---
+++ python-mindmap.mm
@@ -0,0 +1,436 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1576932058370" ID="ID_1034970043" MODIFIED="1576934306687" TEXT="Python">
+<node CREATED="1576932092546" HGAP="-82" ID="ID_1974878246" MODIFIED="1576935380906" POSITION="left" VSHIFT="-140">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Expressions
+    </p>
+    <ul>
+      <li>
+        <font size="2">ont une valeur (p.ex. 2, True, 'cha&#238;ne') </font>
+      </li>
+      <li>
+        <font size="2">imbriqu&#233;es par des parenth&#232;ses</font>
+      </li>
+    </ul>
+  </body>
+</html></richcontent>
+<arrowlink DESTINATION="ID_1974878246" ENDARROW="Default" ENDINCLINATION="0;0;" ID="Arrow_ID_339552458" STARTARROW="None" STARTINCLINATION="0;0;"/>
+<arrowlink DESTINATION="ID_1974878246" ENDARROW="Default" ENDINCLINATION="0;0;" ID="Arrow_ID_1768058532" STARTARROW="None" STARTINCLINATION="0;0;"/>
+<linktarget COLOR="#b0b0b0" DESTINATION="ID_1974878246" ENDARROW="Default" ENDINCLINATION="0;0;" ID="Arrow_ID_339552458" SOURCE="ID_1974878246" STARTARROW="None" STARTINCLINATION="0;0;"/>
+<linktarget COLOR="#b0b0b0" DESTINATION="ID_1974878246" ENDARROW="Default" ENDINCLINATION="0;0;" ID="Arrow_ID_1768058532" SOURCE="ID_1974878246" STARTARROW="None" STARTINCLINATION="0;0;"/>
+<node CREATED="1576932199085" ID="ID_1900995031" MODIFIED="1576932298184">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Bool&#233;enne
+    </p>
+    <p>
+      <font size="3">Vrai (</font><font face="Lucida Console" size="3">True</font><font size="3">) ou Faux (</font><font face="Lucida Console" size="3">False</font><font size="3">)</font>
+    </p>
+  </body>
+</html></richcontent>
+<node CREATED="1576932406176" ID="ID_1108346201" MODIFIED="1576933036003">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <table border="1">
+      <tr>
+        <td>
+          &#160;
+        </td>
+        <th>
+          Vrai <font face="Lucida Console">True</font>
+        </th>
+        <th>
+          Faux <font face="Lucida Console">False</font>
+        </th>
+      </tr>
+      <tr>
+        <th>
+          NON <font face="Lucida Console">not</font>
+        </th>
+        <td>
+          Faux <font face="Lucida Console">False</font>
+        </td>
+        <td>
+          Vrai <font face="Lucida Console">True</font>
+        </td>
+      </tr>
+    </table>
+  </body>
+</html></richcontent>
+</node>
+<node CREATED="1576932737344" ID="ID_7572045" MODIFIED="1576933000444">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <table border="1">
+      <tr>
+        <td>
+          <font face="Lucida Console">ET and</font>
+        </td>
+        <th>
+          Vrai <font face="Lucida Console">True</font>
+        </th>
+        <th>
+          Faux <font face="Lucida Console">False</font>
+        </th>
+      </tr>
+      <tr>
+        <th>
+          Vrai <font face="Lucida Console">True</font>
+        </th>
+        <td>
+          Vrai <font face="Lucida Console">True</font>
+        </td>
+        <td>
+          Faux <font face="Lucida Console">False</font>
+        </td>
+      </tr>
+      <tr>
+        <th>
+          Faux <font face="Lucida Console">False</font>
+        </th>
+        <td>
+          Faux <font face="Lucida Console">False</font>
+        </td>
+        <td>
+          Faux <font face="Lucida Console">False</font>
+        </td>
+      </tr>
+    </table>
+  </body>
+</html></richcontent>
+</node>
+<node CREATED="1576932737344" ID="ID_661664488" MODIFIED="1576933096806">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <table border="1">
+      <tr>
+        <td>
+          <font face="Lucida Console">OU or</font>
+        </td>
+        <th>
+          Vrai <font face="Lucida Console">True</font>
+        </th>
+        <th>
+          Faux <font face="Lucida Console">False</font>
+        </th>
+      </tr>
+      <tr>
+        <th>
+          Vrai <font face="Lucida Console">True</font>
+        </th>
+        <td>
+          Vrai <font face="Lucida Console">True</font>
+        </td>
+        <td>
+          Vrai <font face="Lucida Console">True</font>
+        </td>
+      </tr>
+      <tr>
+        <th>
+          Faux <font face="Lucida Console">False</font>
+        </th>
+        <td>
+          Vrai <font face="Lucida Console">True</font>
+        </td>
+        <td>
+          Faux <font face="Lucida Console">False</font>
+        </td>
+      </tr>
+    </table>
+  </body>
+</html></richcontent>
+</node>
+<node CREATED="1576933463663" ID="ID_227138782" MODIFIED="1576933794747">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Comparaisons
+    </p>
+    <ul>
+      <li>
+        Vrai <font face="Lucida Console">True</font>: <font face="Lucida Console">1 == 1</font>, <font face="Lucida Console">1 != 2</font>, <font face="Lucida Console">3 &lt; 4</font>, <font face="Lucida Console">5 &gt;= 5 &gt; 4</font>
+      </li>
+      <li>
+        Faux <font face="Lucida Console">False</font>: <font face="Lucida Console">1 != 1</font>, <font face="Lucida Console">1 == 2</font>, <font face="Lucida Console">3 &gt;= 4</font>, <font face="Lucida Console">5 &lt; 5 &lt;= 4</font>
+      </li>
+    </ul>
+  </body>
+</html></richcontent>
+</node>
+</node>
+<node CREATED="1576933103485" ID="ID_978302501" MODIFIED="1576933448957">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Num&#233;rique
+    </p>
+    <ul>
+      <li>
+        op&#233;rations de base <font face="Lucida Console">+</font>, <font face="Lucida Console">-</font>, <font face="Lucida Console">*</font>, <font face="Lucida Console">/</font>
+      </li>
+      <li>
+        division enti&#232;re: <font face="Lucida Console">5 // 2</font>&#160;= <font face="Lucida Console">2</font>; <font face="Lucida Console">5 / 2</font>&#160;= <font face="Lucida Console">2.5</font>
+      </li>
+      <li>
+        reste apr&#232;s la division: <font face="Lucida Console">%</font>
+      </li>
+      <li>
+        valeur absolue: <font face="Lucida Console">abs(3)</font>&#160;= <font face="Lucida Console">abs(-3)</font>&#160;= <font face="Lucida Console">3</font>
+      </li>
+    </ul>
+  </body>
+</html></richcontent>
+</node>
+<node CREATED="1576933833650" ID="ID_474046191" MODIFIED="1576933898565" TEXT="Cha&#xee;nes (de caract&#xe8;res)">
+<node CREATED="1576933900216" ID="ID_426738459" MODIFIED="1576933955051">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <font face="Lucida Console">len('abc')</font>&#160;= <font face="Lucida Console">3</font>
+    </p>
+  </body>
+</html></richcontent>
+</node>
+<node CREATED="1576933963134" ID="ID_80996193" MODIFIED="1576934101478">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      &#233;crites <font face="Lucida Console">'abc'</font>, <font face="Lucida Console">&quot;abc&quot;</font>, <font face="Lucida Console">'''abc''' ou </font>&#160;<font face="Lucida Console">&quot;&quot;&quot;abc&quot;&quot;&quot;</font>
+    </p>
+  </body>
+</html></richcontent>
+</node>
+<node CREATED="1576934110556" ID="ID_1431051708" MODIFIED="1576934266779">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <font face="Lucida Console">'abc'.upper()</font>&#160;= <font face="Lucida Console">&quot;ABC&quot;</font>, <font face="Lucida Console">&quot;XYZ&quot;.lower()</font>&#160; = <font face="Lucida Console">'xyz'</font>
+    </p>
+  </body>
+</html></richcontent>
+</node>
+</node>
+</node>
+<node CREATED="1576934394677" HGAP="-132" ID="ID_177287874" MODIFIED="1576935386843" POSITION="right" VSHIFT="95">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Commandes
+    </p>
+    <p>
+      <font size="2">Imbriqu&#233;es les unes dans les autres par des<br />blocs avanc&#233;s vers la droite par des espaces.</font>
+    </p>
+  </body>
+</html>
+</richcontent>
+<node CREATED="1576934617212" ID="ID_1272566261" MODIFIED="1576935152597">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Nommer un bloc: <font face="Lucida Console">def</font>inir une fonction
+    </p>
+    <pre>def nom_du_bloc(parametre):
+    ... code ...</pre>
+  </body>
+</html>
+</richcontent>
+</node>
+<node CREATED="1576934752356" ID="ID_1939272725" MODIFIED="1576934782373" TEXT="R&#xe9;p&#xe9;tition">
+<node CREATED="1576934903459" ID="ID_1908303242" MODIFIED="1576935777387">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      D&#233;finie (<font face="Lucida Console">for</font>)
+    </p>
+    <p>
+      
+    </p>
+    <ul>
+      <li>
+        Le nombre de r&#233;p&#233;titions est connu &#224; l'avance
+      </li>
+      <li>
+        le m&#234;me bloc de code ex&#233;cut&#233; pour tous les<br />&#233;l&#233;ments d'une liste.
+      </li>
+    </ul>
+  </body>
+</html>
+</richcontent>
+<arrowlink DESTINATION="ID_1908303242" ENDARROW="Default" ENDINCLINATION="0;0;" ID="Arrow_ID_619812668" STARTARROW="None" STARTINCLINATION="0;0;"/>
+<linktarget COLOR="#b0b0b0" DESTINATION="ID_1908303242" ENDARROW="Default" ENDINCLINATION="0;0;" ID="Arrow_ID_619812668" SOURCE="ID_1908303242" STARTARROW="None" STARTINCLINATION="0;0;"/>
+<node CREATED="1576935033384" ID="ID_466101524" MODIFIED="1576936363233">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <pre>for caractere in 'abc':
+    ... caractere sera 'a', puis ...
+    ... 'b' et finalement 'c' ...
+    ... dans ce bloc de code ...</pre>
+  </body>
+</html>
+</richcontent>
+<arrowlink DESTINATION="ID_466101524" ENDARROW="Default" ENDINCLINATION="0;0;" ID="Arrow_ID_1093006501" STARTARROW="None" STARTINCLINATION="0;0;"/>
+<linktarget COLOR="#b0b0b0" DESTINATION="ID_466101524" ENDARROW="Default" ENDINCLINATION="0;0;" ID="Arrow_ID_1093006501" SOURCE="ID_466101524" STARTARROW="None" STARTINCLINATION="0;0;"/>
+</node>
+<node CREATED="1576935159715" ID="ID_826649969" MODIFIED="1576935281982">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <pre>for v in [1, 'xy', True, [1, 2]]:
+    ... v sera 1, puis 'xy', puis ...
+    ... True et finalement [1, 2] ...
+    ... dans ce bloc de code ...</pre>
+  </body>
+</html>
+</richcontent>
+</node>
+<node CREATED="1576935289252" ID="ID_1572608930" MODIFIED="1576936401271">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <pre>for x in range(10):
+    ... x sera 0, puis 1, puis 2, ...
+    ... et ainsi de suite jusqu'&#224; ...
+    ... 9 dans ce bloc de code ...</pre>
+  </body>
+</html>
+</richcontent>
+<arrowlink DESTINATION="ID_1572608930" ENDARROW="Default" ENDINCLINATION="0;0;" ID="Arrow_ID_1044970325" STARTARROW="None" STARTINCLINATION="0;0;"/>
+<linktarget COLOR="#b0b0b0" DESTINATION="ID_1572608930" ENDARROW="Default" ENDINCLINATION="0;0;" ID="Arrow_ID_1044970325" SOURCE="ID_1572608930" STARTARROW="None" STARTINCLINATION="0;0;"/>
+</node>
+</node>
+<node CREATED="1576935501632" ID="ID_1408461825" MODIFIED="1576935726464">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Ind&#233;finie (<font face="Lucida Console">while</font>)
+    </p>
+    <p>
+      Avant chaque ex&#233;cution de la boucle<br />une expression bool&#233;enne (la condition)<br />est v&#233;rifi&#233;e: la r&#233;p&#233;tition a lieu tant<br />la condition est vraie
+    </p>
+    <pre>while x &gt; 0:
+    ... code qui ne sera ex&#233;cut&#233; que si x ...
+    ... est plus grand que 0.  Si ce code ...
+    ... modifie x pour devenir 0 ou n&#233;gatif, ...
+    ... la boucle se terminera ...</pre>
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
+<node CREATED="1576935824642" HGAP="16" ID="ID_1448513617" MODIFIED="1576936315164" VSHIFT="-15">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      D&#233;cision (<font face="Lucida Console">if</font>)
+    </p>
+    <pre>if x == 2:
+    ... ce bloc de code n'est ex&#233;cut&#233; ...
+    ... que si x est &#233;gal &#224; 2 ...</pre>
+  </body>
+</html>
+</richcontent>
+<node CREATED="1576935930572" ID="ID_1791486795" MODIFIED="1576936060216">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Sinon (<font face="Lucida Console">else</font>)
+    </p>
+    <pre>if x == 2:
+    ... ce bloc de code n'est ex&#233;cut&#233; ...
+&#160;&#160;&#160;&#160;... que si x est &#233;gal &#224; 2 ...
+else:
+&#160;&#160;&#160;&#160;... ce bloc de code n'est ex&#233;cut&#233; ...
+&#160;&#160;&#160;&#160;... dans tous les autres cas (donc ...
+    ... quand x n'est pas &#233;gal &#224; 2) ...</pre>
+  </body>
+</html>
+</richcontent>
+</node>
+<node CREATED="1576935942036" ID="ID_96707568" MODIFIED="1576936289768">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Sinon Si (<font face="Lucida Console">elif</font>)
+    </p>
+    <pre>if x == 2:
+    ... ce bloc de code n'est ex&#233;cut&#233; ...
+    ... que si x est &#233;gal &#224; 2 ...
+elif x &gt; 1:
+    ... ce bloc de code n'est ex&#233;cut&#233; ...
+    ... que si x n'est pas &#233;gal &#224; 2 ...
+    ... mais plus grand que 1 ...
+else:
+    ... ce bloc de code n'est ex&#233;cut&#233; ...
+    ... dans tous les autres cas (donc ...
+    ... quand x n'est pas &#233;gal &#224; 2 ...
+    ... ni plus grand que 1) ...</pre>
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
+</node>
+</node>
+</map>
